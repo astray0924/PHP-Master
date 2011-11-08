8e569ed397b87aed14c3cf3f6782a7bf5bae28e2
unknown <KyoungRok@KyoungRok-PC.(none)>
2011-11-09 01:41:30 +0900

    trivial

--- index.php
+++ index.php
@@ -17,34 +17,30 @@ $sql = 'SELECT recipes.name, recipes.description, categories.name
 		WHERE chef = :chef
 		AND categories.name = :category_name';
 
-$stmt = $db_conn -> prepare($sql);
+$stmt = $db_conn->prepare($sql);
 
 // bind the chef value, we only want Lorna's recipes
-$stmt -> bindValue(':chef', 'Lorna');
-$stmt -> bindParam(':category_name', $category);
+$stmt->bindValue(':chef', 'Lorna');
+$stmt->bindParam(':category_name', $category);
 
 // starters
 $category = 'Starter';
-$stmt -> execute();
-$starters = $stmt -> fetchAll();
+$stmt->execute();
+$starters = $stmt->fetchAll();
 
 // pudding
 $category = 'Pudding';
-$stmt -> execute();
-$pudding = $stmt -> fetchAll();
+$stmt->execute();
+$pudding = $stmt->fetchAll();
 
 // Getting last inserted ID
 $sql = 'INSERT INTO recipes (name, description, chef, created)
 		VALUES (:name, :description, :chef, NOW())';
-		
+
 $stmt = $db_conn->prepare($sql);
 
 // perform query
-$stmt->execute(array(
-	':name' => 'Weekday Risotto',
-	':description' => 'Creamy rice-based dish, boosted by in-season ingredients. Otherwise known as \'raid-the-fridge risotto\'',
-	':chef' => 'Lorna')
-	);
-	
+$stmt->execute(array(':name' => 'Weekday Risotto', ':description' => 'Creamy rice-based dish, boosted by in-season ingredients. Otherwise known as \'raid-the-fridge risotto\'', ':chef' => 'Lorna'));
+
 echo "New recipe ID: " . $db_conn->lastInsertId();
 ?>
